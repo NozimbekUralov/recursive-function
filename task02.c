457d8db55faa404b02e5bad00a7b1d5be821c73e
NozimbekUralov <nozimurolov1@gmail.com>
2024-12-14 19:44:38 +0500

    task2

--- task02.c
+++ task02.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
 void func(int n){
+    if(n != 1) func(n - 1);
+    printf("%d\n", n);
 
-    if(n % 2 == 0)
-        printf("%d ", n);
-
-    if(n != 1)
-        func(n - 1);
 }
 
 int main(){ 
 
-    func(9);
-
+    func(5);
     return 0;
 }
